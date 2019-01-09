39f45706dc0e59e4e5291d0ff67ab5d0ace3e49b
Taylor <taybart@gmail.com>
2019-01-09 12:27:19 -0700

    Added more info to display

--- display.go
+++ display.go
@@ -8,6 +8,10 @@ import (
 	"strconv"
 )
 
+const (
+	topOffset = 2
+)
+
 func debug(x, y int, format string, v ...interface{}) {
 	fg := termbox.ColorDefault
 	bg := termbox.ColorDefault
@@ -28,7 +32,7 @@ func drawDir(active int, count int, dir []os.FileInfo, offset, width int) {
 		if len(str) > width-4 {
 			str = str[:width-3] + "..."
 		}
-		for len(str) < width {
+		for len(str) < width-1 {
 			str += " "
 		}
 
@@ -46,7 +50,7 @@ func drawDir(active int, count int, dir []os.FileInfo, offset, width int) {
 				fg = termbox.ColorDefault | termbox.AttrBold
 			}
 		}
-		printString(offset, i, width, str, fg, bg)
+		printString(offset, i+topOffset, width, str, fg, bg)
 	}
 }
 
@@ -94,29 +98,30 @@ func draw(dt directoryTree, cd, userinput string) {
 	cr := conf.ColumnRatios
 	cw := conf.ColumnWidth
 
-	if cw < 0 {
-		cw, _ = termbox.Size()
-	}
 	tbwidth, tbheight := termbox.Size()
-	printString(0, tbheight-1, tbwidth,
-		userinput, termbox.ColorDefault, termbox.ColorDefault)
+	if cw < 0 {
+		cw = tbwidth
+	}
+
+	files := readDir(".")
 
 	parentPath := getParentPath(cd)
 	parentFiles := readDir(parentPath)
 	if _, ok := dt[parentPath]; !ok {
 		dt[parentPath] = dt.newDirForParent(cd)
 	}
+
+	// Draw parent dir in first column
 	offset := 0
 	width := int(float64(cr[0]) / 10.0 * float64(cw))
 	drawDir(dt[parentPath].active, 0, parentFiles, offset, width)
-
-	files := readDir(".")
 
 	offset = int(float64(cr[0])/10.0*float64(cw)) +
 		int(float64(cr[1])/10.0*float64(cw))
 	width = int(float64(cr[2]) / 10.0 * float64(cw))
 	count := 0
 	if len(files) > 0 {
+		// Draw child directory or preview file < 100KB in last column
 		if files[dt[cd].active].IsDir() {
 			childPath := cd + "/" + files[dt[cd].active].Name()
 			files := readDir(childPath)
@@ -130,13 +135,49 @@ func draw(dt directoryTree, cd, userinput string) {
 			cmd := exec.Command("cat", n)
 			buf, _ := cmd.Output()
 			buf = buf[:200]
-			printString(offset, 0, width,
+			printString(offset, topOffset, width,
 				string(buf), termbox.ColorDefault, termbox.ColorDefault)
 		}
 	}
+
+	// Draw current dir in middle column
 	offset = int(float64(cr[0]) / 10.0 * float64(cw))
 	width = int(float64(cr[1]) / 10.0 * float64(cw))
 	drawDir(dt[cd].active, count, files, offset, width)
+
+	{
+		// Print user/cd at top
+		// cdFG := termbox.ColorBlue | termbox.AttrBold
+		un := os.Getenv("USER")
+		hn, err := os.Hostname()
+		if err != nil {
+			panic(err)
+		}
+		ustr := un + "@" + hn
+		printString(0, 0, tbwidth, ustr, termbox.ColorGreen, termbox.ColorDefault)
+		// printString(len(ustr)+1, 0, tbwidth, cd, cdFG, termbox.ColorDefault)
+		printString(len(ustr)+1, 0, tbwidth, cd+"/", termbox.ColorBlue, termbox.ColorDefault)
+		f := files[dt[cd].active]
+		name := f.Name()
+		if f.IsDir() {
+			name += "/"
+		}
+		printString(len(ustr)+len(cd)+2, 0, tbwidth, name,
+			termbox.ColorDefault, termbox.ColorDefault)
+	}
+
+	// Print user input or dir info at bottom
+	if len(userinput) > 0 {
+		printString(0, tbheight-1, tbwidth,
+			userinput, termbox.ColorDefault, termbox.ColorDefault)
+	} else {
+		f := files[dt[cd].active]
+		s := fmt.Sprintf("%s %d %s %s",
+			f.Mode(), f.Size(),
+			f.ModTime().Format("Jan 2 15:04"), f.Name())
+		printString(0, tbheight-1, tbwidth,
+			s, termbox.ColorDefault, termbox.ColorDefault)
+	}
 
 	render()
 }
