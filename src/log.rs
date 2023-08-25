4267a1407b45d13f16ae30bacdd4fcfc74a14532
Taylor Bartlett <taybart@gmail.com>
2023-08-25 10:32:58 -0600

    dont log in prod

--- src/log.rs
+++ src/log.rs
@@ -8,14 +8,20 @@ fn open() -> Result<File, String> {
         .map_err(|e| format!("could not open log file {e}"))?;
     Ok(file)
 }
-pub fn error(msg: String) {
-    let mut file = open().expect("open file");
-    file.write_all("[ERROR] ".as_bytes()).expect("write failed");
-    file.write_all(msg.as_bytes()).expect("write failed");
-    file.write_all("\n".as_bytes()).expect("write failed");
-}
-pub fn write(msg: String) {
-    let mut file = open().expect("open file");
-    file.write_all(msg.as_bytes()).expect("write failed");
-    file.write_all("\n".as_bytes()).expect("write failed");
-}
+cfg_if::cfg_if! {
+if #[cfg(debug_assertions)] {
+    pub fn error(msg: String) {
+        let mut file = open().expect("open file");
+        file.write_all("[ERROR] ".as_bytes()).expect("write failed");
+        file.write_all(msg.as_bytes()).expect("write failed");
+        file.write_all("\n".as_bytes()).expect("write failed");
+    }
+    pub fn write(msg: String) {
+        let mut file = open().expect("open file");
+        file.write_all(msg.as_bytes()).expect("write failed");
+        file.write_all("\n".as_bytes()).expect("write failed");
+    }
+} else {
+    pub fn error(msg: String) {}
+    pub fn write(msg: String) {}
+}}
